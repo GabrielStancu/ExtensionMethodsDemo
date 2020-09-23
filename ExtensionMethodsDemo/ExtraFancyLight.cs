4e8a56c5a725b602e31def69b2bb63f2fb81e98f
Gabriel Stancu <gabriel.stancu07@yahoo.com>
2020-09-23 23:07:21 +0300

    project files upload

---
+++ ExtensionMethodsDemo/ExtraFancyLight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsDemo
+{
+    public class ExtraFancyLight : IBlinkingLight, ITimerLight, ILight
+    {
+        private bool isOn;
+        public void SwitchOn() => isOn = true;
+        public void SwitchOff() => isOn = false;
+        public bool IsOn() => isOn;
+        public async Task Blink(int duration, int repeatCount)
+        {
+            Console.WriteLine("Extra Fancy Light starting the Blink function.");
+            await Task.Delay(duration * repeatCount);
+            Console.WriteLine("Extra Fancy Light has finished the Blink function.");
+        }
+        public async Task TurnOnFor(int duration)
+        {
+            Console.WriteLine("Extra Fancy Light starting the timer function.");
+            await Task.Delay(duration);
+            Console.WriteLine("Extra Fancy Light has finished the timer function.");
+        }
+        public override string ToString() => $"The light is {(isOn? "on":"off")}";
+    }
+}
---
+++ ExtensionMethodsDemo/OverheadLight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethodsDemo
+{
+    public class OverheadLight : ILight, ITimerLight, IBlinkingLight
+    {
+        private bool isOn;
+        public bool IsOn() => isOn;
+        public void SwitchOff() => isOn = false;
+        public void SwitchOn() => isOn = true;
+
+        public override string ToString() => $"The light is {(isOn ? "on" : "off")}";
+    }
+}
